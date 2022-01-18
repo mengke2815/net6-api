525218a974cae95b22c6752304381423978391f9
孟可 <410645049@qq.com>
2022-01-18 14:06:36 +0800

    清理代码

--- NET6.Infrastructure/Repositories/OperationLogRepository.cs
+++ NET6.Infrastructure/Repositories/OperationLogRepository.cs
@@ -1,9 +1,10 @@
 ﻿using NET6.Domain.Entities;
+using NET6.Domain.ViewModels;
 using SqlSugar;
 
 namespace NET6.Infrastructure.Repositories
 {
-    public class OperationLogRepository : BaseRepository<OperationLog, OperationLog>
+    public class OperationLogRepository : BaseRepository<OperationLog, OperationLogView>
     {
         public OperationLogRepository(SqlSugarClient sqlSugar) : base(sqlSugar)
         {
