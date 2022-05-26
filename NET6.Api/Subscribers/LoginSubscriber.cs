ffd7bb7541c8d40a2c90bca8f08e21d7402c608f
孟可 <410645049@qq.com>
2022-05-26 15:26:59 +0800

    对事件总线添加redis支持

--- NET6.Api/Subscribers/LoginSubscriber.cs
+++ NET6.Api/Subscribers/LoginSubscriber.cs
@@ -20,6 +20,7 @@ public class LoginSubscriber : IEventSubscriber
     }
 }
 
+#region 对事件总线添加redis支持
 public class RedisEventSource : IEventSource
 {
     /// <summary>
@@ -45,12 +46,13 @@ public class RedisEventSourceStorer : IEventSourceStorer
     readonly SemaphoreSlim _semaphore = new(0);
     public async ValueTask WriteAsync(IEventSource eventSource, CancellationToken cancellationToken)
     {
-        await RedisHelper.LPushAsync("mychccc", eventSource);
+        await RedisHelper.LPushAsync("MyEventBus", eventSource);
         _semaphore.Release();
     }
     public async ValueTask<IEventSource> ReadAsync(CancellationToken cancellationToken)
     {
         await _semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
-        return await RedisHelper.RPopAsync<RedisEventSource>("mychccc");
+        return await RedisHelper.RPopAsync<RedisEventSource>("MyEventBus");
     }
 }
+#endregion
