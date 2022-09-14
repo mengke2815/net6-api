31b799ad126fce8f32614cfd9e1f79fce4208ce5
孟可 <410645049@qq.com>
2022-09-14 21:01:19 +0800

    updated

--- NET6.Api/Subscribers/RedisEventSourceStorer.cs
+++ NET6.Api/Subscribers/RedisEventSourceStorer.cs
@@ -13,7 +13,30 @@ public class RedisEventSourceStorer : IEventSourceStorer
     }
     public async ValueTask<IEventSource> ReadAsync(CancellationToken cancellationToken)
     {
-        await _semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
+        await _semaphore.WaitAsync(cancellationToken);
         return await RedisHelper.RPopAsync<ChannelEventSource>("MyEventBus");
     }
+
+    #region redis实现
+    //readonly Channel<IEventSource> _channel;
+    //public RedisEventSourceStorer(int capacity)
+    //{
+    //    _channel = Channel.CreateBounded<IEventSource>(capacity);
+    //    RedisHelper.SubscribeList("MyEventBus", msg =>
+    //    {
+    //        if (msg.NotNull())
+    //        {
+    //            _channel.Writer.TryWrite(msg.ToObject<ChannelEventSource>());
+    //        }
+    //    });
+    //}
+    //public async ValueTask WriteAsync(IEventSource eventSource, CancellationToken cancellationToken)
+    //{
+    //    await RedisHelper.LPushAsync("MyEventBus", eventSource.ToJson());
+    //}
+    //public async ValueTask<IEventSource> ReadAsync(CancellationToken cancellationToken)
+    //{
+    //    return await _channel.Reader.ReadAsync(cancellationToken);
+    //} 
+    #endregion
 }
